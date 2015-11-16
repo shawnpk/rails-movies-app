12cb0b7ad297dc0e9684a834dd5c6890e64685e8
Shawn Kearney <shawnpk@ineptsoftware.com>
2015-11-16 10:26:39 -0800

    Add individual movie page and links

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
-  get 'movies', to: 'movies#index'
+  root 'movies#index'
+  get 'movies',     to: 'movies#index'
+  get 'movies/:id', to: 'movies#show', as: 'movie'
 
 end
