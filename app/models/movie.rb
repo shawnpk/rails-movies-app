60087284ded67d939e1d158b42a8e7f71513e82e
Shawn Kearney <shawnpk@ineptsoftware.com>
2015-11-17 10:05:20 -0800

    Add validations

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,16 @@
 class Movie < ActiveRecord::Base
 
+  RATINGS = %W(G PG PG-13 R NC-17)
+
+  validates :rating, inclusion: { in: RATINGS }
+  validates :title, :released_on, :duration, presence: true
+  validates :description, length: { minimum: 25 }
+  validates :total_gross, numericality: { greater_than_or_equal_to: 0 }
+  validates :image_file_name, allow_blank: true, format: {
+    with: /\w+\.(gif|jpg|jpeg|png)\z/i,
+    message: 'must be a GIF, JPG, JPEG or PNG file'
+  }
+
   def flop?
     total_gross.blank? || total_gross < 50000000
   end
