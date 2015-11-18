590c8b6d276ee362df9be1650063f459a87fbb47
Shawn Kearney <shawnpk@ineptsoftware.com>
2015-11-18 10:38:41 -0800

    Create average review UI

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -1,2 +1,14 @@
 module ReviewsHelper
+
+  def format_average_stars(movie)
+    if movie.average_stars.nil?
+      content_tag(:strong, 'No Reviews')
+    else
+      pluralize(number_with_precision(
+        movie.average_stars, precision: 1), 
+        'Star')
+      # '* ' * movie.average_stars.round
+    end
+  end
+
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -20,4 +20,8 @@ class Movie < ActiveRecord::Base
     where('released_on <= ?', Time.now).order('released_on desc')
   end
 
+  def average_stars
+    reviews.average(:stars)
+  end
+
 end
